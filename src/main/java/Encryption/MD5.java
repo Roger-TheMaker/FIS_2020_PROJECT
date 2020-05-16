2944cc9db4fd2cf932f5d71ad0ba622aa0321ed0
Roman Bogdan <bogdanrionut71@gmail>
2020-05-16 19:20:55 +0300

    AAA-24 something failed trying again

--- src/main/java/Encryption/MD5.java
+++ src/main/java/Encryption/MD5.java
@@ -4,7 +4,7 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-// Java program to calculate MD5 hash value
+
 public class MD5 {
     public static String getMd5(String input) {
         try {
