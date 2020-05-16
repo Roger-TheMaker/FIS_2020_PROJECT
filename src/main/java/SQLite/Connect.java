1c24930305fa1892695bad53a7f76081a3158122
Roman Bogdan <bogdanrioonut71@gmail.com>
2020-05-16 04:07:04 +0300

    added jdbc connection class for .db files

---
+++ src/main/java/SQLite/Connect.java
@@ -0,0 +1,37 @@
+
+
+package SQLite;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    private static Connection conn = null;
+
+    public static void connect(String nameDB) {
+
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:" + nameDB;
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void closeConnection() {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+}
