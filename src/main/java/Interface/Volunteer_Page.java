09296050776939878536ab1b486f23f460d3544c
Roger <rogernuica3@gmail.com>
2020-05-20 13:14:01 +0300

    AAA-34 found a way to distribute posts dynamically on the interface

--- src/main/java/Interface/Volunteer_Page.java
+++ src/main/java/Interface/Volunteer_Page.java
@@ -1,10 +1,11 @@
 package Interface;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Volunteer_Page extends  JDialog {
+public class Volunteer_Page extends JDialog {
 
     private JPanel contentPane;
     private JPanel buttons_Panel;
@@ -12,15 +13,18 @@ public class Volunteer_Page extends  JDialog {
     private JButton AddButton;
     private JButton V_Button;
     private JTextField welcomeSummonerTextField;
-    private JTextField announcementsTextField;
     private JButton buttonOK;
     private JButton buttonCancel;
+    //MAI VEDEM DACA ADAUGAM SI UN MESAJ SUS CU ANNOUNCEMENTS
 
     public Volunteer_Page(){
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
 
+        contentPane =new JPanel();
+
+        posts_Panel.setLayout(new FlowLayout());
 
         V_Button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -31,6 +35,22 @@ public class Volunteer_Page extends  JDialog {
             public void actionPerformed(ActionEvent e) {
 
 
+                JTextField text =new JTextField("Text");
+                text.setPreferredSize( new Dimension( 320, 26) );
+
+                JTextField username =new JTextField("Username");
+                username.setPreferredSize( new Dimension( 80, 26) );
+
+                JButton b = new JButton("Respond Post");
+                b.setBounds(500, 500, 100, 20);
+                posts_Panel.add(username);
+                posts_Panel.add(text);
+                posts_Panel.add(b);
+
+                posts_Panel.revalidate();;
+                posts_Panel.validate();
+
+                //aici salvam butonul in baza de date odata cu adaugarea pe interfata
             }
         });
     }
