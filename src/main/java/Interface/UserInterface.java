c031d2d38fe7761080409c3f0fa3260e27e1073b
Roger <rogernuica3@gmail.com>
2020-06-18 17:40:55 +0300

    Userinterface updated, delete finished AAA-18 AAA-31

--- src/main/java/Interface/UserInterface.java
+++ src/main/java/Interface/UserInterface.java
@@ -1,14 +1,15 @@
 package Interface;
 
 import Network.*;
-import SQLite.CreateTable;
-import SQLite.Insert;
-import SQLite.Select;
+import SQLite.*;
+import org.javatuples.Triplet;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 public class UserInterface extends JDialog {
 
@@ -20,9 +21,14 @@ public class UserInterface extends JDialog {
     private JTextField welcomeSummonerTextField;
     private JTextField PostTextField;
     private JScrollPane PostBox;
+    private JButton deletePostButton;
+    private JButton refreshButton;
     private JButton buttonOK;
     private JButton buttonCancel;
-    private int RowID = 1;
+    public static int RowID = 1;
+    private int set = 0;
+    private static Semaphore semaphore = new Semaphore(1);
+    private ArrayList<Triplet<JTextField,JTextField,JButton>> post = new ArrayList<Triplet<JTextField,JTextField,JButton>>();
 
     public UserInterface(){
         setContentPane(contentPane);
@@ -32,23 +38,36 @@ public class UserInterface extends JDialog {
         posts_Panel.setLayout(new FlowLayout());
         PostTextField.setText("Write Something...");
 
-        String tableContent = "id integer PRIMARY KEY, USERNAME text NOT NULL, HELP_MESSAGE text NOT NULL, IP_ADDRESS text NOT NULL, UNIQUE(USERNAME, HELP_MESSAGE) ";
+        String tableContent = "id integer PRIMARY KEY, USERNAME text NOT NULL, HELP_MESSAGE text NOT NULL, IP_ADDRESS text NOT NULL,UNIQUE(HELP_MESSAGE)";
         CreateTable.CreateTable("test.db","POSTS",tableContent);
 
 
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
+                try {
+                    semaphore.acquire();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 while(true) {
                     //Row[0] = username
                     //Row[1] = post
                     //Row[2] = ip
+                    if(RowID == 1) {
+                        if(BroadcastServer.message != null && set == 1) {
+                            deletePost(BroadcastServer.message);
+                            set = 0;
+                        }
+                    }
                     String Row[] = Select.getRow("test.db", "SELECT * FROM POSTS WHERE id = " + String.valueOf(RowID));
                     printPost(Row[0], Row[1], Row[2]);
                     try {
                         Thread.sleep(500);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
+                    } finally {
+                        semaphore.release();
                     }
                 }
             }
@@ -63,15 +82,24 @@ public class UserInterface extends JDialog {
         });
         AddButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String message = addPost();
+                String message = "MESSAGE"+addPost();
+                BroadcastClient.run(message);
 
-                Thread thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        BroadcastClient.sendBroadcast(message);
-                    }
-                });
-                thread.start();
+
+
+            }
+        });
+        deletePostButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                set = 1;
+                BroadcastClient.run("DELETE"+UserService.user);
+
+            }
+        });
+        refreshButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
             }
         });
@@ -92,15 +120,50 @@ public class UserInterface extends JDialog {
 
         System.out.println(valueList);
 
-        Insert.Insert("test.db","POSTS",parameterList,valueList);
+        //   Insert.Insert("test.db","POSTS",parameterList,valueList);
 
         return valueList;
+
+    }
+
+    private void removeComponent(Triplet<JTextField,JTextField,JButton> components) {
+        for(Object obj : components) {
+            posts_Panel.remove((Component) obj);
+        }
+    }
+
+    public void deletePost(String user) {
+        String sql_command = "DELETE FROM POSTS WHERE USERNAME = " + "\'"+ user +"\'";
+        Delete.DeleteEntry("test.db","POSTS",sql_command);
+        System.out.println("POST DELETED");
+
+        Component[] componentList = posts_Panel.getComponents();
+
+//Loop through the components
+        for(int i = 0; i < post.size(); i++) {
+            for(Object obj : post.get(i)) {
+                if(obj instanceof JTextField && ((JTextField) obj).getText().equals(UserService.user)) {
+                    removeComponent(post.get(i));
+                    break;
+                }
+
+            }
+
+        }
+
+//IMPORTANT
+        posts_Panel.revalidate();
+        posts_Panel.repaint();
+        RowID = 1;
+
 
     }
 
     private void printPost(String usrname, String post, String ipAddress) {
         if(usrname == null || post == null || ipAddress == null)
             return;
+
+
 
         JTextField PostTextField = new JTextField("");
         String help_message= post; //HELP_MESSAGE COLLUMN
@@ -110,10 +173,11 @@ public class UserInterface extends JDialog {
 
         JTextField usernameTextField =new JTextField("");
         usernameTextField.setPreferredSize( new Dimension( 80, 26) );
-            usernameTextField.setText( UserService.user);
+        usernameTextField.setText( UserService.user);
 
 
         JButton RespondButton = new JButton("Respond Post");
+        this.post.add(Triplet.with(usernameTextField,PostTextField,RespondButton));
         RespondButton.setBounds(500, 500, 100, 20);
         RespondButton.addActionListener(new ActionListener() {
             @Override
@@ -136,6 +200,8 @@ public class UserInterface extends JDialog {
         posts_Panel.validate();
 
         RowID++;
+
+        return;
     }
 
 
