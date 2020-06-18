b8dab5ceec8ca06535e5636457fefa6f9dba9218
Roger <rogernuica3@gmail.com>
2020-06-18 17:33:13 +0300

    Brandcast Server updated - correlated with the UserInterface for Delete AAA-31

--- src/main/java/Network/BroadcastServer.java
+++ src/main/java/Network/BroadcastServer.java
@@ -1,8 +1,10 @@
 package Network;
 
+import Interface.UserInterface;
 import Interface.UserService;
 import Network.DiscoveryThread;
 import SQLite.CreateTable;
+import SQLite.Delete;
 import SQLite.Insert;
 //import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import java.io.IOException;
@@ -12,7 +14,7 @@ import java.util.logging.Logger;
 
 public class BroadcastServer implements Runnable {
     private static DatagramSocket socket;
-
+    public static String message;
     @Override
     public void run() {
         try {
@@ -24,16 +26,26 @@ public class BroadcastServer implements Runnable {
                 DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
                 socket.receive(packet);
 
-                System.out.println(getClass().getName() + ">>>Packet received; data: " + new String(packet.getData()));
 
-                String message = new String(packet.getData()).trim();
 
-                String tableContent = "id integer PRIMARY KEY, USERNAME text NOT NULL, HELP_MESSAGE text NOT NULL, IP_ADDRESS text NOT NULL, UNIQUE(USERNAME, HELP_MESSAGE) ";
-                CreateTable.CreateTable("test.db","POSTS",tableContent);
+                message = new String(packet.getData()).trim();
+                System.out.println(getClass().getName() + ">>>Packet received; data: " + new String(message));
+                if(message.contains("MESSAGE")) {
+                    message = message.replaceAll("MESSAGE", "");
+                    String tableContent = "id integer PRIMARY KEY, USERNAME text NOT NULL, HELP_MESSAGE text NOT NULL, IP_ADDRESS text NOT NULL, UNIQUE(HELP_MESSAGE)";
+                    CreateTable.CreateTable("test.db", "POSTS", tableContent);
 
-                String parameterList = "USERNAME, HELP_MESSAGE, IP_ADDRESS";
-                Insert.Insert("test.db","POSTS",parameterList,message);
-
+                    String parameterList = "USERNAME, HELP_MESSAGE, IP_ADDRESS";
+                    Insert.Insert("test.db", "POSTS", parameterList, message);
+                }
+                else if(message.contains("DELETE")) {
+                    message = message.replaceAll("DELETE","");
+                    String sql_command = "DELETE FROM POSTS WHERE USERNAME = " + "\'"+ message +"\'";
+                    Delete.DeleteEntry("test.db","POSTS",sql_command);
+                    System.out.println("POST DELETED");
+                    UserInterface.RowID = 1;
+                }
+                System.out.println(getClass().getName() + ">>>Packet received; data: " + new String(message));
             }
         } catch (IOException ex) {
             Logger.getLogger(DiscoveryThread.class.getName()).log(Level.SEVERE, null, ex);
