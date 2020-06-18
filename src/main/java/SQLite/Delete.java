3de0ecb5c93f5c7411f0d9cb05de4625603db30c
Roger <rogernuica3@gmail.com>
2020-06-18 16:57:19 +0300

    Delete class created - connection with the database realized AAA-30

---
+++ src/main/java/SQLite/Delete.java
@@ -0,0 +1,22 @@
+package SQLite;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Delete {
+
+    public static void DeleteEntry(String nameDB, String tableName,String sql_command  ) {
+        Connection conn = Connect.connect(nameDB);
+        try {
+            PreparedStatement pstmt  = conn.prepareStatement(sql_command);
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            Connect.closeConnection();
+        }
+    }
+
+}
