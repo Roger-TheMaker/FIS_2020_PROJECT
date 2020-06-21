c6716b580be18e80fc05c2c6a48466ba7b8c8617
Roger <rogernuica3@gmail.com>
2020-06-21 03:20:24 +0300

    First test for the method connect from the Connect class- Not A DB File

--- src/main/java/SQLite/Connect.java
+++ src/main/java/SQLite/Connect.java
@@ -2,6 +2,8 @@
 
 package SQLite;
 
+import Exceptions.Not_A_DB_File;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,7 +11,13 @@ import java.sql.SQLException;
 public class Connect {
     private static Connection conn = null;
 
-    public synchronized static Connection connect(String nameDB) {
+    public synchronized static Connection connect(String nameDB) throws  Not_A_DB_File{
+
+        if(!nameDB.endsWith(".db")){
+
+            throw  new Not_A_DB_File();
+        }
+
 
         try {
             // db parameters
